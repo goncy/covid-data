f8ad92d0e63b04c4ebfea2a80a7e899c9f047d64
manuhank <manuhank@gmail.com>
2020-07-19 12:22:06 -0300

    update endpoint

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import {Case} from "./types";
 
 export default {
   fetch: (): Promise<Case[]> =>
-    fetch(`https://corona.lmao.ninja/countries`)
+    fetch(`https://corona.lmao.ninja/v3/covid-19/countries `)
       .then((res) => res.json())
       .catch(() => Promise.reject("Error fetching data")),
 };
